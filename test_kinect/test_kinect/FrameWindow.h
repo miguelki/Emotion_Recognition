946d326f587074a30d315f6e8dfdecf274e23d53
Kim-Adeline Miguel <kimadeline.miguel@gmail.com>
2012-10-09 14:14:59 +0200

    nettoyage + rangement

--- test_kinect/test_kinect/FrameWindow.h
+++ test_kinect/test_kinect/FrameWindow.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <WinUser.h>
+#define W_WDH 800
+#define W_HGT 600
+
 
 /** UI **/
 
@@ -9,10 +12,11 @@ class FrameWindow
 
 private :
 	HWND hwnd; // return value after window creation
-	ID2D1Factory* pD2DFactory;
-	ID2D1HwndRenderTarget *pD2DTarget;
+
+	ID2D1Factory*pD2DFactory;
+	ID2D1HwndRenderTarget* pD2DTarget;
+	ID2D1Bitmap* pD2D1Frame;
 	ID2D1SolidColorBrush* pBlackBrush;
-	ID2D1Bitmap *pD2D1Frame;
 
 	static LRESULT CALLBACK WndProc(
 	HWND hWnd,
@@ -24,17 +28,17 @@ private :
 public:
 	FrameWindow(void);
 	~FrameWindow(void);
+	HRESULT createWindow();
+	HRESULT createFactory();
+	HRESULT createRenderTarget(HWND hw);
+	HRESULT init(HWND hw);
 	HRESULT drawWindow(); // draw window frame & background
 	HRESULT drawFrame(BYTE* frame, UINT32 pitch,UINT32 width,UINT32 height); // draw input frames	
-	void RunMessageLoop();
 
 	/** Getters and setters **/
 
 	HWND getHandle() {return hwnd;}
 	void setHandle(HWND wh) {hwnd = wh;}
-
-	/** Events handlers **/
-	    HRESULT onRender();
-
-    void onResize(UINT width,UINT height);
+	ID2D1HwndRenderTarget* getRenderTarget() {return pD2DTarget;}
+	
 };
