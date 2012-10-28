8a150c302d32d0c5f27fe4596063f0c01d205335
Kim-Adeline Miguel <kimadeline.miguel@gmail.com>
2012-10-28 12:59:37 +0100

    lbp sur 1 image fixe
    youhou lbp fonctionne, il y a une image de sortie, au cas où vous auriez
    la flemme de générer le code :)

---
+++ test_lbp/test_lbp/stdafx.h
@@ -0,0 +1,16 @@
+// stdafx.h : fichier Include pour les fichiers Include système standard,
+// ou les fichiers Include spécifiques aux projets qui sont utilisés fréquemment,
+// et sont rarement modifiés
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <cv.h>
+#include <highgui.h>
+#include <iostream>
+
+
+
+// TODO: faites référence ici aux en-têtes supplémentaires nécessaires au programme
---
+++ test_lbp/test_lbp/test_lbp.cpp
@@ -0,0 +1,144 @@
+// test_lbp.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+
+/** Lors du passage en MVC, il serait peut-être plus judicieux de transformer ces définitions en membres d'une classe **/
+
+#define NB_ROWS 7
+#define NB_COLS 6
+
+using namespace cv;
+using namespace std;
+
+Mat lbpProcess(Mat src); // normalize facial features using Local Binary Patterns
+vector<unsigned int> histogramProcess(Mat src); // compute histogram out of normalized image
+
+int main (int argc, char* argv[]) {
+
+	Mat i_mat, o_mat;
+	vector<unsigned int> hist;
+	string i_file = "visage.png"; // input image
+	string o_file = "output_" + i_file;
+
+	/** load input image, greyscale mode **/
+	i_mat = imread(i_file, 0);
+	o_mat.create(i_mat.rows, i_mat.cols, CV_8UC1);
+
+	if (i_mat.data == NULL) {
+		cout << "Could not load image ! "  << endl;
+		return -1;
+	} else {
+
+		/** Divide image in areas **/
+		int img_w = i_mat.cols;
+		int img_h = i_mat.rows;
+
+		int wdh = (int) floor((double)(img_w / NB_COLS));
+		int hgt = (int) floor((double)(img_h / NB_ROWS));
+
+		/** prepare vector **/
+		hist.clear();
+
+		for (int j = 0; j < NB_ROWS; j++) {
+			for (int i = 0; i < NB_COLS; i++) {
+
+				int start_w = i * wdh - 1;
+				int end_w = (i+1) * wdh + 1;
+				int start_h = j * hgt - 1;
+				int end_h = (j+1) * hgt + 1;
+
+				if (j == 0)
+					start_h = 0;
+
+				if (j == NB_ROWS - 1)
+					end_h = img_h;
+
+				if (i == 0)
+					start_w = 0;
+
+				if (i == NB_COLS - 1)
+					end_w = img_w;
+
+				/** Process sub-area **/
+
+				/*cout << "Area #" << j*(NB_ROWS-1)+i << " : (" << i << "," << j << ")" << endl;
+				cout << "Top left coordinates : (" << start_w << "," << start_h << ")" << endl;
+				cout << "Bottom right coordinates : (" << end_w << "," << end_h << ")" << endl;
+				cout << "------" << endl;
+				*/
+				CvRect roi = cvRect(start_w, start_h, (end_w - start_w), (end_h - start_h));
+
+				Mat res = lbpProcess(i_mat(roi));
+				//cout << "test 1 " << j*(NB_ROWS-1)+i << endl; 
+				/** Compute histogram **/
+				vector<unsigned int> tmp = histogramProcess(res);
+
+				for (unsigned int k = 0; k < tmp.size(); k++) {
+					hist.push_back(tmp[i]);
+				}
+				//cout << "test 2 " << j*(NB_ROWS-1)+i << endl;
+				/** Copy new area into output matrix **/
+				Mat tmp_m = o_mat(roi);
+				res.copyTo(tmp_m);
+			}
+		}
+
+		o_mat = lbpProcess(i_mat);
+
+		/** Save output image and histogram **/
+		// TODO
+
+		imwrite(o_file, o_mat);
+
+		/** Display both images **/
+		namedWindow( "Input image", CV_WINDOW_AUTOSIZE );
+		namedWindow( "Output image", CV_WINDOW_AUTOSIZE );
+		imshow("Input image", i_mat);
+		imshow("Output image", o_mat);
+		waitKey(0);
+		cvDestroyAllWindows();
+	}
+	return 0;
+
+}
+
+
+Mat lbpProcess(Mat src) {
+
+	Mat dst = Mat::zeros(src.rows-2, src.cols-2, CV_8UC1);
+
+	for(int i=1;i<src.rows-1;i++) {
+		for(int j=1;j<src.cols-1;j++) {
+			unsigned char center = src.at<unsigned char>(i,j);
+			unsigned char code = 0;
+			code |= (src.at<unsigned char>(i-1,j-1) > center) << 7;
+			code |= (src.at<unsigned char>(i-1,j) > center) << 6;
+			code |= (src.at<unsigned char>(i-1,j+1) > center) << 5;
+			code |= (src.at<unsigned char>(i,j+1) > center) << 4;
+			code |= (src.at<unsigned char>(i+1,j+1) > center) << 3;
+			code |= (src.at<unsigned char>(i+1,j) > center) << 2;
+			code |= (src.at<unsigned char>(i+1,j-1) > center) << 1;
+			code |= (src.at<unsigned char>(i,j-1) > center) << 0;
+
+			dst.at<unsigned char>(i-1,j-1) = code;
+		}
+	}
+
+	return dst;
+}
+
+/** Improve it elegantly with a recursive algorithm **/
+
+vector<unsigned int> histogramProcess(Mat src) {
+	vector<unsigned int> hist (256,0);
+
+	for (int i = 0; i < src.rows; i++) {
+		for (int j = 0; j < src.cols; j++) {
+			int n = (int) src.at<unsigned char>(i, j);
+			hist[n] += 1;
+		}
+	}
+
+	return hist;
+}
