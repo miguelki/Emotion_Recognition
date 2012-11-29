466e3dad750846e4c8cfad77fa342663fec73d24
Julia Vigo <tit.tete@gmail.com>
2012-11-29 12:09:03 +0100

    Geometry-based method

--- test_svm/test_svm/test_svm.cpp
+++ test_svm/test_svm/test_svm.cpp
@@ -2,49 +2,63 @@
 
 #include "stdafx.h"
 
-#define MAX_SZ 2500
-#define NB_FEAT 780
+#define MAX_SZ 5800
+#define NB_FEAT 420
+#define NB_CLASS 7
+#define NB_ELEM_PER_CLASS 12
 
 using namespace std;
 
+const std::string classLbls[NB_CLASS] = {"neutral", "afraid", "angry", "disgusted", "happy", "sad", "surprised"};
+
 struct svm_problem* read_file(string train_file);
 struct svm_parameter* fill_params(struct svm_problem* prob);
-double classify_accuracy(struct svm_model* model, struct svm_problem* test_d);
+double overall_accuracy(struct svm_model* model, struct svm_problem* test_d);
+vector<double> class_accuracy(struct svm_model* model, struct svm_problem* test_d); 
 
 int main(int argc, char* argv[])
 {
 	int existing_model = 1; // 1 if model already exists, 0 otherwise
 
-	string model_file = "model_mnist";
-	string train_file = "svm/mnist.tr";
-	string test_file = "svm/mnist.t";
+	string model_file = "model_kdef";
+	string train_file = "svm/kdef_sc2.tr";
+	string test_file = "svm/kdef_sc2.t";
 
 	struct svm_model *model;
-	cout << "reading train data" << endl;
-	struct svm_problem *prob = read_file(train_file);
+
 	cout << "reading test data" << endl;
 	struct svm_problem *test_d = read_file(test_file);
-	struct svm_parameter *param;
 
 	if (!existing_model) {
+		cout << "reading train data" << endl;
 
-		param = fill_params(prob);
+		struct svm_problem* prob = read_file(train_file);
+		struct svm_parameter* param = fill_params(prob);
 
 		cout << "parameters fetched - training in progress" << endl;
-		
+
+		/** Cross-validation and re-training **/
+		//double* target = new double[prob->l];
+		//svm_cross_validation(prob, param, 5, target);
+		//prob->y = target;
+
+		//cout << "cross-validation done" << endl;
+
 		/** Train model **/
 		model = svm_train(prob, param);
 
-		cout << "training done";
+		cout << "training done" << endl;
 
 		/** Save model into a file **/
-
-		cout << "- saving model" << endl;
+		cout << "saving model" << endl;
 		if (svm_save_model(model_file.c_str(), model) == -1)
 			exit(-1);
 
 		cout << "model saved - classification" << endl;
-
+		
+		//delete[] target;
+		delete param;
+		delete prob;		
 	} else {
 		/** Load model **/
 		cout << "Loading model" << endl;
@@ -54,15 +68,16 @@ int main(int argc, char* argv[])
 	}
 
 	/** Classify and test accuracy **/
-	cout << "Accuracy : " <<  classify_accuracy(model, test_d) << endl;
+	//	cout << "Overall accuracy : " <<  overall_accuracy(model, test_d) << endl;
+	vector<double> acc = class_accuracy(model, test_d);
+	cout << "-- Accuracy --" << endl;
+	for (unsigned int i = 0; i < NB_CLASS; i++) 
+		cout << classLbls[i] << " expression : " << acc[i] << endl;
+	cout << "Overall accuracy : " <<  acc[NB_CLASS] << endl;
 
 	/** Release memory **/
-	if (!existing_model) {
-		free(prob);
-		free(param);
-		free(test_d);
-	}
-	free(model);
+	delete test_d;
+	svm_free_and_destroy_model(&model);
 
 	system("pause");
 	return 0;
@@ -79,14 +94,13 @@ struct svm_problem* read_file(string train_file) {
 	/** Open file **/
 	infile.open (train_file, ifstream::in);
 
-	while (infile.good()) {		
+	while (infile.getline(buf, MAX_SZ)) {		
 		struct svm_node* ln_vec;
 		int nb, nb_elem = 0;
 		size_t lbl_p, cur_p, nxt_p;
 
-		infile.getline(buf, MAX_SZ);
 		string tmp(buf);
-		
+
 		// First value is label
 		lbl_p = tmp.find_first_of(" \t");
 		labels.push_back(atof(tmp.substr(0, lbl_p).c_str()));
@@ -121,7 +135,7 @@ struct svm_problem* read_file(string train_file) {
 		// don't forget to add a pair <-1, -1> to mark the end of the vector
 		ln_vec[nb_elem-1].index = -1;
 		ln_vec[nb_elem-1].value = -1;
-	
+
 		nb_d++; // new node :) 
 
 		// push it into values vector
@@ -130,7 +144,7 @@ struct svm_problem* read_file(string train_file) {
 
 	/** Close file **/
 	infile.close();
-
+	//cout << "number of lines read : " << nb_d << endl;
 	/** Copy and assign structures to svm_problem so it won't be wiped out at the end of the function **/
 	prob->l = nb_d;		
 	prob->y = new double[nb_d];
@@ -147,35 +161,35 @@ struct svm_problem* read_file(string train_file) {
 struct svm_parameter* fill_params(struct svm_problem* prob) {
 	struct svm_parameter* params = new struct svm_parameter;
 
-			/** Fill up parameter structure : each value is taken from default values in the Matlab implementation**/
-		params->svm_type = C_SVC;
-		params->kernel_type = LINEAR;
-		params->gamma = 1/NB_FEAT;
-		params->degree = 3;
-		params->coef0 = 0;
-		params->nr_weight = 0;
-		params->eps = 0.001;
-		params->cache_size = 100;
-		params->C = 1;
-		params->shrinking = 1;
-		params->probability = 0;
+	/** Fill up parameter structure : each value is taken from default values in the Matlab implementation**/
+	params->svm_type = C_SVC;
+	params->kernel_type = RBF;
+	params->gamma = 0.00048828125;//1/NB_FEAT;//0.0078125;//1;
+	params->degree = 3;
+	params->coef0 = 0;
+	params->nr_weight = 0;
+	params->eps = 0.001;
+	params->cache_size = 100;
+	params->C = 32.0;//1;
+	params->shrinking = 1;
+	params->probability = 0;
 
-		/** Check parameters **/
+	/** Check parameters **/
 
-		cout << "check parameters : ";
-		char* str = _strdup(svm_check_parameter(prob, params));
+	cout << "check parameters : ";
+	char* str = _strdup(svm_check_parameter(prob, params));
 
-		if (str != NULL)
-			cout << str << endl;
-		else
-			cout << "no problem !" << endl;
+	if (str != NULL)
+		cout << str << endl;
+	else
+		cout << "no problem !" << endl;
 
-		free(str);
+	free(str);
 
 	return params;
 }
 
-double classify_accuracy(struct svm_model* model, struct svm_problem* test_d) {
+double overall_accuracy(struct svm_model* model, struct svm_problem* test_d) {
 	double accuracy = 0;
 	int nb_elemts = test_d->l;
 
@@ -187,5 +201,28 @@ double classify_accuracy(struct svm_model* model, struct svm_problem* test_d) {
 
 	return accuracy/nb_elemts;
 	/*cout << "# of elements : " << nb_elemts << " - accuracy : " << accuracy << endl;
-	 return accuracy;*/
+	return accuracy;*/
+}
+
+vector<double> class_accuracy(struct svm_model* model, struct svm_problem* test_d) {
+	vector<double> class_acc;
+	int nb_elemts = test_d->l;
+
+	class_acc.assign(NB_CLASS+1, 0);
+
+	for (int i = 0; i < nb_elemts; i++) {
+		double predict = svm_predict(model, *(test_d->x+i));
+		if (predict == *(test_d->y+i)) {
+			class_acc[(int)predict]++;
+		}
+	}
+
+	for (int j = 0; j < NB_CLASS; j++) {
+		class_acc[j] /= NB_ELEM_PER_CLASS;
+		class_acc[NB_CLASS] += class_acc[j];
+	}
+
+	class_acc[NB_CLASS] /= NB_CLASS;
+
+	return class_acc;
 }
